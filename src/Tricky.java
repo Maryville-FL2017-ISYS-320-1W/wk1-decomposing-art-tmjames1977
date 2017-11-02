2a5368e8fa979f9ab9980f911883929a5c3aef8e
tmjames1977 <33112355+tmjames1977@users.noreply.github.com>
2017-11-01 23:34:57 -0500

    Performed tasks 1 - 5 for assignment.

--- src/Tricky.java
+++ src/Tricky.java
@@ -1,17 +1,38 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s): Timothy James
+  	Date: 11/1/2017
 */
 
-// 1. Your predicted output 
- 
+/* 1. It will output message 1, message 2, message 1, 
+ * done w/message 2, and done w/ main.
+*/
 
 public class Tricky {
-
+    public static void main(String[] args) {
+        message1();
+        message2();
+        System.out.println("Done with main.");
+    }
+    
+    public static void message1() {
+        System.out.println("This is message1.");
+    }
+    
+    public static void message2() {
+        System.out.println("This is message2.");
+        message1();
+        System.out.println("Done with message 2.");
+    }
 }
 
-// 3. Were you correct? Explain any differences
+/* 3. I was correct. At first I thought it was going 
+ * to just output message 1, message 2, done w/message 2, 
+ * and done w/ main. I did not catch the hidden "message1();"
+ * in the second static void message at first glance but after
+ * looking a little closer I suspect that this was the "Tricky"
+ * part.
+ */
  
  
 
